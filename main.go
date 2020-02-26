9b854521631542a3e7298ad156fe640893037168
nanagami1369 <nanagami1369@gmail.com>
2020-02-26 17:26:11 +0900

    htmlを表示できるようにした

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("Views/*.html")
+
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
