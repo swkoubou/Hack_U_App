d50a153353e7b6905ad20b65b3b0a9e820121120
okapon1210 <okapon1210@gmail.com>
2020-02-27 03:01:35 +0900

    cssとjsを切り分けた

---
+++ Views/Assets/index.js
@@ -0,0 +1,24 @@
+function initMap() {
+    var myLatLng = {lat: 35.487027, lng: 139.342440};
+    var map = new google.maps.Map(document.getElementById('area'), {
+        center: myLatLng,
+        zoom: 18
+    });
+
+    var contentString = '<p>storename</p>'+
+        '<p>text</p>'+
+        '<a href="https://google.com">Go google!</a>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
