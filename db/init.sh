616bdc0a416fe7c681a20a316c8be71a88cf41b8
nanagami1369 <nanagami1369@gmail.com>
2020-02-27 23:00:42 +0900

    どこにいてもスクリプトが実行できるように修正

--- db/init.sh
+++ db/init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mysql -u$HACK_U_App_MYSQL_USER -p$HACK_U_App_MYSQL_PASSWORD hack_u_db < ./create_table.sql
-mysql -u$HACK_U_App_MYSQL_USER -p$HACK_U_App_MYSQL_PASSWORD hack_u_db < ./data.sql
+pushd $(dirname $0)
+mysql -u$HACK_U_App_MYSQL_USER -p$HACK_U_App_MYSQL_PASSWORD hack_u_db <./create_table.sql
+mysql -u$HACK_U_App_MYSQL_USER -p$HACK_U_App_MYSQL_PASSWORD hack_u_db <./data.sql
+popd
