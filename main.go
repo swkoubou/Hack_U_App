eab8296449ea25d46954bc094858a35fdaf91fde
nanagami1369 <nanagami1369@gmail.com>
2020-02-29 02:05:16 +0900

    ルーティングを追加

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/swkoubou/Hack_U_App/Applications"
+	"github.com/swkoubou/Hack_U_App/controllers"
+	Infrastructure "github.com/swkoubou/Hack_U_App/db"
 	"net/http"
 	"os"
 )
 
 func main() {
+	controller := controllers.NewWheelchairRentalLocationsSearchController(Applications.NewWheelchairRentalLocationsSearchApplication(Infrastructure.NewMysql()))
 	router := gin.Default()
 
 	router.Static("./Assets", "./Views/Assets")
@@ -18,5 +22,11 @@ func main() {
 		})
 	})
 
+	router.GET("/allLocation", controller.SearchRange)
+
+	router.GET("/search", controller.SearchTags)
+
+	router.GET("/allTag", controller.GetAllTags)
+	
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
