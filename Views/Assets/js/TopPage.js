e04979791e6d2f0e49dc417490751f496e551cb3
okapon1210 <okapon1210@gmail.com>
2020-03-01 07:12:09 +0900

    タグを表示できるようにした

--- Views/Assets/js/TopPage.js
+++ Views/Assets/js/TopPage.js
@@ -1,5 +1,5 @@
 function menu() {
-    const button = document.querySelector('.drbutton');
+    const button = document.getElementById("menubutton");
     const menu = document.querySelector('.menu');
 
     button.addEventListener('click',function () {
@@ -7,3 +7,43 @@ function menu() {
     });
 }
 menu();
+function refine() {
+    const button = document.getElementById("refinebutton");
+    const menu = document.querySelector('.refine');
+
+    button.addEventListener('click',function () {
+        menu.classList.toggle('hide');
+    });
+}
+refine();
+function addrefineformtag() {
+    var alltagjson = "http://localhost:8080/allTag";
+    const form = document.querySelector(".refine");
+    fetch(alltagjson)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myjson){
+            var i;
+            var quantity = Object.keys(myjson.tags).length;
+            var tagcheck = {};
+            var taglabel = {};
+
+            for (i = 0;i < quantity;i++){
+                tagcheck[i] = document.createElement("input");
+                taglabel[i] = document.createElement("label");
+                
+                tagcheck[i].type = "checkbox";
+                tagcheck[i].value = myjson.tags[i].TagId;
+                tagcheck[i].id = myjson.tags[i].Name;
+                //tagcheck[i].classList.add("hide");
+                
+                taglabel[i].htmlFor = myjson.tags[i].Name;
+                taglabel[i].innerHTML = myjson.tags[i].Name;
+                taglabel[i].classList.add("taglabel");
+                
+                form.appendChild(tagcheck[i]);
+                form.appendChild(taglabel[i])
+            }
+    });
+}addrefineformtag();
