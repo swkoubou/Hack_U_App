d886647ce42de8ae6b7c62890075782a88d36230
nanagami1369 <nanagami1369@gmail.com>
2020-03-19 23:49:46 +0900

    場所検索システムの追加

--- Views/Assets/js/Map.js
+++ Views/Assets/js/Map.js
@@ -15,4 +15,28 @@ function initMap() {
         content: contentString
     });
 
+    //検索システムの追加
+    const input = document.querySelector("#SearchBox-Input");
+    const options = {
+        type: ['establishment'],
+    };
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete.bindTo('bounds', map);
+
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        const place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("その場所はありません: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(18);
+        }
+    });
 }
