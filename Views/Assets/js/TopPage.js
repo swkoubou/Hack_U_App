ee6acaa9dfeddf0f9ee47f4fe7e235a4e884a0f6
okapon1210 <okapon1210@gmail.com>
2020-04-01 14:09:30 +0900

    コードを整形した

--- Views/Assets/js/TopPage.js
+++ Views/Assets/js/TopPage.js
@@ -1,5 +1,5 @@
 class Tag {
-    constructor(TagId,TagName) {
+    constructor(TagId, TagName) {
         this.Checkbox = document.createElement("input");
         this.Label = document.createElement("label");
         this.Checkbox.type = "checkbox";
@@ -17,25 +17,26 @@ class Tag {
     }
 }
 
-class LocationInformation{
+class LocationInformation {
     constructor(Json) {
         this.Position = Json.Location;
         this.Name = Json.Name;
         this.Address = Json.Address;
-        this.DetailedPage = "/Page?p="+Json.LocationId;
+        this.DetailedPage = "/Page?p=" + Json.LocationId;
     }
 }
 
-function HeaderButton(HeadButtonId,displayPageId) {
+function HeaderButton(HeadButtonId, displayPageId) {
     const Button = document.querySelector("#" + HeadButtonId);
-    const Menu = document.querySelector("#"+ displayPageId);
+    const Menu = document.querySelector("#" + displayPageId);
 
-    Button.addEventListener("click",function () {
+    Button.addEventListener("click", function () {
         Menu.classList.toggle("Hide");
     });
 }
-HeaderButton("MenuButton","Menu");
-HeaderButton("TagFilterButton","TagFilter");
+
+HeaderButton("MenuButton", "Menu");
+HeaderButton("TagFilterButton", "TagFilter");
 
 function AddFilterFormTag() {
     const AllTagURL = "http://localhost:8080/allTag";
@@ -44,25 +45,26 @@ function AddFilterFormTag() {
         .then(function (response) {
             return response.json();
         })
-        .then(function (TagJson){
+        .then(function (TagJson) {
             const TagQuantity = Object.keys(TagJson.tags).length;
             const Tags = [];
 
-            for (let i = 0;i < TagQuantity;i++){
-                Tags[i] = new Tag(TagJson.tags[i].TagId,TagJson.tags[i].Name);
+            for (let i = 0; i < TagQuantity; i++) {
+                Tags[i] = new Tag(TagJson.tags[i].TagId, TagJson.tags[i].Name);
 
                 Form.appendChild(Tags[i].Checkbox);
                 Form.appendChild(Tags[i].Label);
             }
         });
 }
+
 AddFilterFormTag();
 
-function CheckConfirmation(){
+function CheckConfirmation() {
     const Checked = [];
     const CheckBox = document.FilterForm.FilterCheck;
-    for(let i = 0;i < CheckBox.length;i++){
-        if (CheckBox[i].checked){
+    for (let i = 0; i < CheckBox.length; i++) {
+        if (CheckBox[i].checked) {
             Checked.push(CheckBox[i].value);
         }
     }
@@ -72,7 +74,7 @@ function CheckConfirmation(){
 function FilteringDisplay() {
     const SelectTag = CheckConfirmation();
     const SearchTagsURL = "http://localhost:8080/search?tags=";
-    const Menu =document.querySelector("#TagFilter");
+    const Menu = document.querySelector("#TagFilter");
     fetch(SearchTagsURL + SelectTag)
         .then(function (response) {
             return response.json();
@@ -80,17 +82,17 @@ function FilteringDisplay() {
         .then(function (FilteredJson) {
             const LocationQuantity = Object.keys(FilteredJson.location).length;
             const LocationInformations = [];
-            for (let i = 0;i < LocationQuantity;i++){
+            for (let i = 0; i < LocationQuantity; i++) {
                 LocationInformations[i] = new LocationInformation(FilteredJson.location[i]);
             }
             InitMarker(LocationInformations);
         });
     Menu.classList.add("Hide");
-    //名前、住所、詳細ページのURL
 }
 
 let map;
 let MarkerArray;
+
 function initMap() {
     let myLatLng = {lat: 35.487027, lng: 139.342440};
     map = new google.maps.Map(document.getElementById("Area"), {
@@ -109,8 +111,8 @@ function InitMarker(LocationInformations) {
     });
 
     MarkerArray.clear();
-    for (let i = 0;i < LocationInformations.length;i++){
-        MarkersInformation ={
+    for (let i = 0; i < LocationInformations.length; i++) {
+        MarkersInformation = {
             position: {
                 lat: LocationInformations[i].Position.Lat,
                 lng: LocationInformations[i].Position.Lng
@@ -118,14 +120,14 @@ function InitMarker(LocationInformations) {
             map: map,
             title: LocationInformations[i].Name
         };
-        ContentString ={
-            content: "<p>"+LocationInformations[i].Name+"</p>"+
-                "<p>"+LocationInformations[i].Address+"</p>"+
-                "<a href=\'"+LocationInformations[i].DetailedPage+"\'>詳細ページ</>"
+        ContentString = {
+            content: "<p>" + LocationInformations[i].Name + "</p>" +
+                "<p>" + LocationInformations[i].Address + "</p>" +
+                "<a href=\'" + LocationInformations[i].DetailedPage + "\'>詳細ページ</>"
         };
         MarkerArray.push(new google.maps.Marker(MarkersInformation));
         InfoWindows[i] = new google.maps.InfoWindow(ContentString);
-        MarkerArray.getAt(i).addListener("click", function() {
+        MarkerArray.getAt(i).addListener("click", function () {
             InfoWindows[i].open(map, MarkerArray.getAt(i));
         });
     }
